50e9415c8b3dacef05b9aa066f7210c997dc29f4
Erieltxu <ld.aldecoa@gmail.com>
2024-05-31 10:36:23 +0200

    reset y contador puestos

--- game/first-function.js
+++ game/first-function.js
@@ -1,23 +1,41 @@
 // Parte uno: Funciones principales de vasos y botón de play//
-
 document.addEventListener("DOMContentLoaded", () => {
     const cups = document.querySelectorAll(".cup");
     const ball = document.getElementById("ball");
     const playButton = document.getElementById("start-button");
     const resetButton = document.getElementById("reset");
+    const countdownElement = document.getElementById("countdown");
+    const hourglass = document.getElementById("imagen-hourglass");
     let ballPosition = 0;
+    let gameStarted = false;
+    let countdownInterval;
 
     playButton.addEventListener("click", () => {
-        raiseAllCups();
-        ballPosition = Math.floor(Math.random() * 3);  
+        if (!gameStarted) {
+            startCountdown(10);
+            startRotation(); 
+            raiseAllCups();
+            ballPosition = Math.floor(Math.random() * 3);
+            gameStarted = true;
+            playButton.disabled = true;
+            resetButton.disabled = false;
+        }
     });
 
     resetButton.addEventListener("click", () => {
         resetGame();
+        stopRotation(); 
+        clearInterval(countdownInterval); 
+        gameStarted = false;
+        playButton.disabled = false;
+        resetButton.disabled = true;
+        ballPosition = 0;
+        countdownElement.textContent = "";
     });
 
     cups.forEach((cup, index) => {
         cup.addEventListener("click", () => {
+            if (!gameStarted) return; 
             raiseCup(index); 
             if (ballPosition === index) {
                 movCups(); 
@@ -61,6 +79,30 @@ document.addEventListener("DOMContentLoaded", () => {
         });
         ball.style.display = "none";
     }
+
+    
+    function startRotation() {
+        hourglass.classList.add("rotating");
+    }
+
+    
+    function stopRotation() {
+        hourglass.classList.remove("rotating");
+    }
+
+    
+    function startCountdown(duration) {
+        let timeRemaining = duration;
+        countdownElement.textContent = timeRemaining;
+
+        countdownInterval = setInterval(() => {
+            timeRemaining--;
+            countdownElement.textContent = timeRemaining;
+
+            if (timeRemaining <= 0) {
+                clearInterval(countdownInterval);
+                stopRotation(); 
+            }
+        }, 1000);
+    }
 });
-
-
