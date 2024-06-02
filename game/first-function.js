c6f829761637264245f40a0d66f36fde85884a08
Leire Del Hoyo <167010312+Erieltxu@users.noreply.github.com>
2024-06-02 18:54:39 +0200

    Revert "Features/alerts"

--- game/first-function.js
+++ game/first-function.js
@@ -6,15 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const resetButton = document.getElementById("reset");
     const countdownElement = document.getElementById("countdown");
     const hourglass = document.getElementById("imagen-hourglass");
-    const scoreDiv = document.getElementById('score');
-    let score = 0;
     let ballPosition = 0;
     let gameStarted = false;
     let countdownInterval;
 
     playButton.addEventListener("click", () => {
-        if (!gameStarted) { 
+        if (!gameStarted) {
+            startCountdown(3);
+            startRotation(); 
             raiseAllCups();
+            ballPosition = Math.floor(Math.random() * 3);
             gameStarted = true;
             playButton.disabled = true;
             resetButton.disabled = false;
@@ -29,24 +30,19 @@ document.addEventListener("DOMContentLoaded", () => {
         playButton.disabled = false;
         resetButton.disabled = true;
         ballPosition = 0;
-        countdownElement.textContent = "5";
+        countdownElement.textContent = "";
     });
 
     cups.forEach((cup, index) => {
         cup.addEventListener("click", () => {
             if (!gameStarted) return; 
-            raiseCup(index);
-            stopRotation(); 
-            clearInterval(countdownInterval); 
+            raiseCup(index); 
             if (ballPosition === index) {
-                revealBall();
-                updateScore(true);
+                movCups(); 
                 setTimeout(() => {
                     alert("Correcto! Encontraste la bolita.");
                 }, 1200);
             } else {
-                raiseCup(ballPosition); // Si ha elegido mal se levanta el vaso correcto y se muestra la bola
-                revealBall();
                 setTimeout(() => {
                     alert("Incorrecto, Intenta de nuevo.");
                 }, 1200);
@@ -54,11 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    function moveBall() { // Funcion que cambia de posicion la bola para que sea mas descriptivo
-        ballPosition = Math.floor(Math.random() * 3);
-    }
-
-    function revealBall() { // Cambiado nombre de la funcion para que sea mas descriptivo con lo que hace
+    function movCups() {
         ball.style.display = "block";
         ball.style.top = cups[ballPosition].offsetTop + cups[ballPosition].offsetHeight - (ball.offsetHeight*2) + "px";
         ball.style.left = cups[ballPosition].offsetLeft + (cups[ballPosition].offsetWidth / 2) - (ball.offsetWidth / 2) + "px";
@@ -68,16 +60,12 @@ document.addEventListener("DOMContentLoaded", () => {
         cups.forEach(cup => {
             cup.style.transform = "translateY(-70px)";
         });
-        revealBall(); // Para mostrar la bola al levantar todos los vasos cuando se inicia el juego
 
         setTimeout(() => {
             cups.forEach(cup => {
                 cup.style.transform = "translateY(0)";
             });
             ball.style.display = "none";
-            moveBall(); // Se mueve la bola despues de bajar los vasos y ocultar la bolita
-            startCountdown(5); // Se inicia el contador luego de bajar los vasos
-            startRotation();
         }, 1000);
     }
 
@@ -110,19 +98,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (timeRemaining <= 0) {
                 clearInterval(countdownInterval);
-                stopRotation();
-                gameStarted = false;
-                setTimeout(() => {
-                    alert("Se te ha acabado el tiempo, presiona RESET para volver a jugar");
-                }, 300);
+                stopRotation(); 
             }
         }, 1000);
-    }
-
-    function updateScore(win) {
-        if (win) {
-            score += 10;
-        }
-        scoreDiv.textContent = `Score: ${score}`;
     }
 });
